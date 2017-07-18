476ee8e824522485057e43173eb6b1e73d586ba5
lxk <lixink@8531.cn>
2017-07-18 10:39:48 +0800

    add my Subscription

---
+++ subscription/src/main/java/com/daily/news/subscription/more/my/MyStore.java
@@ -0,0 +1,47 @@
+package com.daily.news.subscription.more.my;
+
+import com.daily.news.subscription.more.column.Column;
+import com.daily.news.subscription.more.column.ColumnContract;
+
+import org.reactivestreams.Publisher;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Flowable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
+
+/**
+ * Created by lixinke on 2017/7/17.
+ */
+
+public class MyStore implements ColumnContract.Store<List<Column>> {
+    @Override
+    public Flowable<List<Column>> getFlowable(String url) {
+        return Flowable.timer(400, TimeUnit.MILLISECONDS)
+                .flatMap(new Function<Long, Publisher<List<Column>>>() {
+                    @Override
+                    public Publisher<List<Column>> apply(@NonNull Long aLong) throws Exception {
+                        Random random = new Random();
+                        List<Column> columns = new ArrayList<>();
+                        for (int i = 0; i < 30; i++) {
+                            Column column = new Column();
+                            column.article_count = random.nextInt();
+                            column.subscribe_count = random.nextInt();
+                            column.name = "搜索数据" + random.nextInt();
+                            column.subscribed = random.nextBoolean();
+                            columns.add(column);
+                        }
+
+                        return Flowable.just(columns);
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.newThread());
+    }
+}
