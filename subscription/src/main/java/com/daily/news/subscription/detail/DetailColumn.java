65684de13389a83e6f61a13ec53e4617076e6871
lxk <lixink@8531.cn>
2017-07-18 10:49:17 +0800

    detail model

---
+++ subscription/src/main/java/com/daily/news/subscription/detail/DetailColumn.java
@@ -0,0 +1,21 @@
+package com.daily.news.subscription.detail;
+
+import com.daily.news.subscription.subscription.SubscriptionBean;
+
+import java.util.List;
+
+/**
+ * Created by lixinke on 2017/7/18.
+ */
+
+public class DetailColumn {
+    private String uid;
+    private String name;
+    private String pic_url;
+    private int subscribe_count;
+    private int article_count;
+    private boolean subscribed;
+    private String background_url;
+    private String description;
+    private List<SubscriptionBean.DataBean.ArticleBean> elements;
+}
---
+++ subscription/src/main/java/com/daily/news/subscription/detail/DetailResponse.java
@@ -0,0 +1,16 @@
+package com.daily.news.subscription.detail;
+
+import java.util.List;
+
+/**
+ * Created by lixinke on 2017/7/18.
+ */
+
+public class DetailResponse {
+
+
+    private int code;
+    private String message;
+    private String request_id;
+    private List<DetailColumn> detailColumns;
+}
