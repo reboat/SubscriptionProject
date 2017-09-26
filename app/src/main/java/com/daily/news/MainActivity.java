5fb94f3fb080194d37b6c1b3e3954ef56ac9672d
lixinke <lixink@8531.cn>
2017-09-26 19:49:29 +0800

    修改首页添加切换夜间模式

--- app/src/main/java/com/daily/news/MainActivity.java
+++ app/src/main/java/com/daily/news/MainActivity.java
@@ -1,82 +1,45 @@
 package com.daily.news;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.view.MenuItem;
+import android.support.annotation.IdRes;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.RadioGroup;
 
 import com.daily.news.subscription.home.SubscriptionFragment;
 import com.zjrb.core.common.base.BaseActivity;
+import com.zjrb.core.db.ThemeMode;
 
 public class MainActivity extends BaseActivity {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            Fragment recommend = fragmentManager.findFragmentByTag("subscription");
-            FragmentTransaction transaction = null;
-
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setVisibility(View.VISIBLE);
-                    mTextMessage.setText("切换夜间模式");
-                    if (recommend != null) {
-                        transaction = fragmentManager.beginTransaction();
-                        transaction.hide(recommend);
-                        transaction.commit();
-                    }
-                    return true;
-                case R.id.navigation_dashboard:
-                    if (recommend == null) {
-
-                        transaction = fragmentManager.beginTransaction();
-                        transaction.add(R.id.content, SubscriptionFragment.newInstance(), "subscription");
-                        transaction.commit();
-
-
-                    } else {
-                        transaction = fragmentManager.beginTransaction();
-                        transaction.show(recommend);
-                        transaction.commit();
-                    }
-                    mTextMessage.setVisibility(View.GONE);
-
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setVisibility(View.VISIBLE);
-                    mTextMessage.setText(R.string.title_notifications);
-                    if (recommend != null) {
-                        transaction = fragmentManager.beginTransaction();
-                        transaction.hide(recommend);
-                        transaction.commit();
-                    }
-                    return true;
-            }
-            return false;
-        }
-
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        findViewById(R.id.night_model).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ThemeMode.setUiMode(!ThemeMode.isNightMode());
+            }
+        });
 
+        RadioGroup group = (RadioGroup) findViewById(R.id.radio_group);
+        group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                switch (checkedId) {
+                    case R.id.home:
+                        break;
+                    case R.id.sub:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, SubscriptionFragment.newInstance()).commit();
+                        break;
+                    case R.id.develop:
+                        break;
+                }
+            }
+        });
     }
+
 
 }
