bbc37be3e232740f168f4334cb51677ec8ad2649
lixinke <lixink@8531.cn>
2017-10-23 15:33:28 +0800

    去除订阅提示Toast

--- subscription/src/main/java/com/daily/news/subscription/home/RecommendColumnFragment.java
+++ subscription/src/main/java/com/daily/news/subscription/home/RecommendColumnFragment.java
@@ -1,7 +1,5 @@
 package com.daily.news.subscription.home;
 
-import android.widget.Toast;
-
 import com.daily.news.subscription.more.column.ColumnFragment;
 import com.daily.news.subscription.more.column.ColumnResponse;
 
@@ -12,6 +10,6 @@ import com.daily.news.subscription.more.column.ColumnResponse;
 public class RecommendColumnFragment extends ColumnFragment {
     @Override
     public void subscribeSuc(ColumnResponse.DataBean.ColumnBean bean) {
-        Toast.makeText(getContext(),bean.subscribed?"订阅成功":"取消订阅成功",Toast.LENGTH_SHORT).show();
+//        Toast.makeText(getContext(),bean.subscribed?"订阅成功":"取消订阅成功",Toast.LENGTH_SHORT).show();
     }
 }
