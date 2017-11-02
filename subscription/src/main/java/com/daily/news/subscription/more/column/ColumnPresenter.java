9499a2a87d6d091e5a573f77d140b316c7161430
lixinke <lixink@8531.cn>
2017-11-02 09:16:12 +0800

    destroy 时cancel task

--- subscription/src/main/java/com/daily/news/subscription/more/column/ColumnPresenter.java
+++ subscription/src/main/java/com/daily/news/subscription/more/column/ColumnPresenter.java
@@ -4,26 +4,24 @@ import com.daily.news.subscription.detail.RxException;
 import com.daily.news.subscription.subscribe.SubscribePresenter;
 import com.zjrb.core.api.base.APIBaseTask;
 import com.zjrb.core.api.callback.APICallBack;
+import com.zjrb.core.common.manager.APICallManager;
 
 import java.util.List;
-
-import io.reactivex.disposables.CompositeDisposable;
 
 /**
  * Created by lixinke on 2017/7/17.
  */
 
 public class ColumnPresenter extends SubscribePresenter implements ColumnContract.Presenter {
+    private static final String COLUMN_TASK_TAG = "column_task";
     private ColumnContract.View mView;
     private ColumnContract.Store mStore;
-    private CompositeDisposable mDisposable;
 
     public ColumnPresenter(ColumnContract.View view, ColumnContract.Store store) {
         super(view, store);
         mView = view;
         mView.setPresenter(this);
         mStore = store;
-        mDisposable = new CompositeDisposable();
     }
 
     @Override
@@ -45,23 +43,24 @@ public class ColumnPresenter extends SubscribePresenter implements ColumnContrac
             public void onError(String errMsg, int errCode) {
                 super.onError(errMsg, errCode);
                 mView.hideProgressBar();
-                mView.showError(new RxException(errMsg,errCode));
+                mView.showError(new RxException(errMsg, errCode));
             }
         });
         if (task != null) {
             task.bindLoadViewHolder(mView.getProgressBar()).exe(params);
+            task.setTag(COLUMN_TASK_TAG);
         }
     }
 
 
     @Override
     public void unsubscribe() {
-        mDisposable.clear();
+        APICallManager.get().cancel(COLUMN_TASK_TAG);
     }
 
     public void refreshData(List<ColumnResponse.DataBean.ColumnBean> recommend_list) {
-        ColumnResponse.DataBean dataBean=new ColumnResponse.DataBean();
-        dataBean.elements=recommend_list;
+        ColumnResponse.DataBean dataBean = new ColumnResponse.DataBean();
+        dataBean.elements = recommend_list;
         mView.updateValue(dataBean);
     }
 }
