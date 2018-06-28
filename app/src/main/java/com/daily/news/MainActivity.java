aedff46ff24f2e575d17bdd49d67924657e41fa6
高阳· 甄 <627312987@qq.com>
2018-06-28 09:27:54 +0800

    修复订阅更多栏目分类在字数处于临界点时无法显示省略号的问题

--- app/src/main/java/com/daily/news/MainActivity.java
+++ app/src/main/java/com/daily/news/MainActivity.java
@@ -2,14 +2,19 @@ package com.daily.news;
 
 import android.os.Bundle;
 import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatDelegate;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.daily.news.subscription.home.SubscriptionFragment;
 import com.zjrb.core.common.base.BaseActivity;
 import com.zjrb.core.db.ThemeMode;
 import com.zjrb.core.nav.Nav;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends BaseActivity {
 
@@ -18,6 +23,19 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView txt = (TextView) findViewById(R.id.testtxt);
+        txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
+//                txt.setLines(1);
+                txt.setText("分类测试测");
+
+            }
+        });
+
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
 
         final Button button = (Button) findViewById(R.id.night_model);
         button.setText(ThemeMode.isNightMode()?"夜间模式":"正常模式");
